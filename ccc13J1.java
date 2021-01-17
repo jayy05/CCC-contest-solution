dee054041f3ec43c5c6f08d9a772cac59cbd5d47
jayy05 <77591314+jayy05@users.noreply.github.com>
2021-01-17 15:17:13 -0800

    Create ccc13J1.java

---
+++ ccc13J1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+     Scanner in = new Scanner(System.in);
+     int young = in.nextInt();
+     int middle = in.nextInt();
+     System.out.println((middle-young)+middle);
+
+  }
+}
